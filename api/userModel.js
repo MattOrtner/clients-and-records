40e77896ebfe9557dea37f5d0359c4de88c2b573
Matt Ortner <matthewortner@gmail.com>
2025-04-21 17:06:49 -0400

    remove logs

--- api/userModel.js
+++ api/userModel.js
@@ -155,7 +155,6 @@ const deleteClient = (params) => {
 
 //update a client record
 const updateClient = (params, body) => {
-  console.log("body", body);
   const fields = [];
   const values = [];
   const clientId = params.clientId;
@@ -170,7 +169,7 @@ const updateClient = (params, body) => {
   query +=
     fields.join(", ") +
     ` WHERE id = ${clientId} AND user_id = ${userId} RETURNING *`;
-  console.log("query", query);
+
   return new Promise(function (resolve, reject) {
     pool.query(query, values, (error, results) => {
       if (error) {
